c6c103e6254c494bafd054ea6083a85e9f0d3be3
vasiliy <basil.anenkov@yandex.ru>
2023-08-26 15:00:31 +0300

    start do log server

--- sandboxes/sandbox 5/src/main.cpp
+++ sandboxes/sandbox 5/src/main.cpp
@@ -1,33 +1,73 @@
-#include <boost/beast.hpp>
-#include <iostream>
+#include <boost/log/trivial.hpp>     // для BOOST_LOG_TRIVIAL
+#include <boost/log/core.hpp>        // для logging::core
+#include <boost/log/expressions.hpp> // для выражения, задающего фильтр
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/console.hpp>
+#include <boost/date_time.hpp>
 
-namespace beast = boost::beast;
-namespace http = beast::http;
-namespace sys = boost::system;
+#include <string_view>
 
-int main(int argc, char* argv[]) {
-    using namespace http;
-    using namespace std::literals;
+using namespace std::literals;
+namespace logging = boost::log;
+namespace keywords = boost::log::keywords;
+namespace sinks = boost::log::sinks;
 
-    if (argc != 2) {
-        std::cout << "Usage: "sv << argv[0] << " <file name>"sv << std::endl;
-        return EXIT_FAILURE;
-    }
+BOOST_LOG_ATTRIBUTE_KEYWORD(line_id, "LineID", unsigned int)
+BOOST_LOG_ATTRIBUTE_KEYWORD(timestamp, "TimeStamp", boost::posix_time::ptime)
 
-    response<file_body> res;
-    res.version(11);  // HTTP/1.1
-    res.result(status::ok);
-    res.insert(field::content_type, "text/plain"sv);
+BOOST_LOG_ATTRIBUTE_KEYWORD(file, "File", std::string)
+BOOST_LOG_ATTRIBUTE_KEYWORD(line, "Line", int)
 
-    file_body::value_type file;
+void InitBoostLogFilter()
+{
+    logging::core::get()->set_filter(
+        logging::trivial::severity >= logging::trivial::info);
+}
 
-    if (sys::error_code ec; file.open(argv[1], beast::file_mode::read, ec), ec) {
-        std::cout << "Failed to open file "sv << argv[1] << std::endl;
-        return EXIT_FAILURE;
-    }
+void MyFormatter(logging::record_view const& rec, logging::formatting_ostream& strm) {
+    // Выводить LineID стало проще.
+    strm << rec[line_id] << ": ";
 
-    res.body() = std::move(file);
-    // Метод prepare_payload заполняет заголовки Content-Length и Transfer-Encoding
-    // в зависимости от свойств тела сообщения
-    res.prepare_payload();
+    // Момент времени приходится вручную конвертировать в строку.
+    // Для получения истинного значения атрибута нужно добавить
+    // разыменование. 
+    auto ts = *rec[timestamp];
+    strm << to_iso_extended_string(ts) << ": ";
+
+    // Выводим уровень, заключая его в угловые скобки.
+    strm << "<" << rec[logging::trivial::severity] << "> ";
+
+    // Выводим само сообщение.
+    strm << rec[logging::expressions::smessage];
+}
+
+int main()
+{
+    InitBoostLogFilter();
+
+    // logging::add_file_log(
+    //     keywords::file_name = "sample_%N.log",
+    //     keywords::format = "[%TimeStamp%]: %Message%",
+    //     keywords::open_mode = std::ios_base::app | std::ios_base::out,
+    //     // ротируем по достижению размера 10 мегабайт
+    //     keywords::rotation_size = 10 * 1024 * 1024,
+    //     // ротируем ежедневно в полдень
+    //     keywords::time_based_rotation = sinks::file::rotation_at_time_point(12, 0, 0));
+
+    logging::add_file_log(
+        keywords::file_name = "sample.log",
+        keywords::format = &MyFormatter);
+
+    logging::add_console_log(
+        std::clog,
+        keywords::format = "[%TimeStamp%]: %Message%",
+        keywords::auto_flush = true);
+
+    BOOST_LOG_TRIVIAL(trace) << "Сообщение уровня trace"sv;
+    BOOST_LOG_TRIVIAL(debug) << "Сообщение уровня debug"sv;
+    BOOST_LOG_TRIVIAL(info) << "Сообщение уровня info"sv;
+    BOOST_LOG_TRIVIAL(warning) << "Сообщение уровня warning"sv;
+    BOOST_LOG_TRIVIAL(error) << "Сообщение уровня error"sv;
+    BOOST_LOG_TRIVIAL(fatal) << "Сообщение уровня fatal"sv;
 }
