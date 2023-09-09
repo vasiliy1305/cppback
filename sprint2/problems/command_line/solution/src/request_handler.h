f806998a9bd00f2ec8ce96a6d3370eb43f6716d8
va <basil.anenkov@yandex.ru>
2023-09-09 09:46:43 -0400

    добавил тикер , но не применял его

--- sprint2/problems/command_line/solution/src/request_handler.h
+++ sprint2/problems/command_line/solution/src/request_handler.h
@@ -4,6 +4,63 @@
 
 namespace http_handler
 {
+    class Ticker : public std::enable_shared_from_this<Ticker> {
+public:
+    using Strand = net::strand<net::io_context::executor_type>;
+    using Handler = std::function<void(std::chrono::milliseconds delta)>;
+
+    // Функция handler будет вызываться внутри strand с интервалом period
+    Ticker(Strand strand, std::chrono::milliseconds period, Handler handler)
+        : strand_{strand}
+        , period_{period}
+        , handler_{std::move(handler)} {
+    }
+
+    void Start() {
+        net::dispatch(strand_, [self = shared_from_this()] {
+            self->SetLastTick(Clock::now());
+            self->ScheduleTick();
+        });
+    }
+    void SetLastTick(std::chrono::steady_clock::time_point last_tick)
+    {
+        last_tick_ = last_tick;
+    }
+
+private:
+    void ScheduleTick() {
+        assert(strand_.running_in_this_thread());
+        timer_.expires_after(period_);
+        timer_.async_wait([self = shared_from_this()](sys::error_code ec) {
+            self->OnTick(ec);
+        });
+    }
+
+    void OnTick(sys::error_code ec) {
+        using namespace std::chrono;
+        assert(strand_.running_in_this_thread());
+
+        if (!ec) {
+            auto this_tick = Clock::now();
+            auto delta = duration_cast<milliseconds>(this_tick - last_tick_);
+            last_tick_ = this_tick;
+            try {
+                handler_(delta);
+            } catch (...) {
+            }
+            ScheduleTick();
+        }
+    }
+
+    using Clock = std::chrono::steady_clock;
+
+    Strand strand_;
+    std::chrono::milliseconds period_;
+    net::steady_timer timer_{strand_};
+    Handler handler_;
+    std::chrono::steady_clock::time_point last_tick_;
+};
+
     using namespace std::literals;
 
     // Ответ, тело которого представлено в виде файла
